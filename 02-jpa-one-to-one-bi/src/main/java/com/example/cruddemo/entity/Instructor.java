4f52e8f9e63c4360a416d2c8badcaa1e3880f48e
vinay-parekh <bvp0802@gmail.com>
2023-10-21 23:18:59 +0530

    bidirectional one-to-one mapping

---
+++ 02-jpa-one-to-one-bi/src/main/java/com/example/cruddemo/entity/Instructor.java
@@ -0,0 +1,48 @@
+package com.example.cruddemo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="instructor")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", instructorDetail=" + instructorDetail +
+                '}';
+    }
+}
